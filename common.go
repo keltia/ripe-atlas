5de9f2a86c342a8aa83df9f2dcacdf76aa41059b
Ollivier Robert <roberto@keltia.net>
2016-05-29 00:26:53 +0200

    Functions about authentication.

--- common.go
+++ common.go
@@ -5,10 +5,34 @@
 
 package atlas
 
-import (
-
-)
+import ()
+import "github.com/bndr/gopencils"
 
 const (
 	apiEndpoint = "https://atlas.ripe.net/api/v2/"
 )
+
+var (
+	// APIUser is the RIPE username
+	APIUser string
+	// APIPassword is the RIPE user password
+	APIPassword string
+)
+
+// SetAuth stores the credentials for later use
+func SetAuth(user, pwd string) {
+	APIUser = user
+	APIPassword = pwd
+}
+
+// WantAuth returns either a BasicAuth or nil depending on stored credentials
+func WantAuth() (auth *gopencils.BasicAuth) {
+	if APIUser == "" || APIPassword == "" {
+		return nil
+	}
+	auth = &gopencils.BasicAuth{
+		Username: APIUser,
+		Password: APIPassword,
+	}
+	return auth
+}
