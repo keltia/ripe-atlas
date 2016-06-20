5ab4d3a65c44a34a9541d574bc0a1078783853a3
Ollivier Robert <roberto@keltia.net>
2016-06-20 18:32:19 +0200

    Placeholders for the various measurements.

--- measurements.go
+++ measurements.go
@@ -1,1 +1,59 @@
 package atlas
+
+import (
+	"errors"
+)
+
+const (
+	allTypes = []string{
+		"dns",
+		"ntp",
+		"ping",
+		"sslcert",
+		"traceroute",
+		"wifi",
+	}
+)
+
+var ErrInvalidMeasurementType = errors.New("invalid measurement type")
+
+// checkType verify that the type is valid
+func checkType(d Definition) (valid bool) {
+	valid = false
+	for _, t := range allTypes {
+		if d.Type == t {
+			valid = true
+			break
+		}
+	}
+	return
+}
+
+// DNS creates a measurement
+func DNS(d Definition) (m *Measurement, err error) {
+	if checkType(d) || d.Type != "dns" {
+		err = ErrInvalidMeasurementType
+		return
+	}
+	return
+}
+
+// NTP creates a measurement
+func NTP(d Definition) (m *Measurement, err error) {
+	return
+}
+
+// Ping creates a measurement
+func Ping(d Definition) (m *Measurement, err error) {
+	return
+}
+
+// SSLCert creates a measurement
+func SSLCert(d Definition) (m *Measurement, err error) {
+	return
+}
+
+// Traceroute creates a measurement
+func Traceroute(d Definition) (m *Measurement, err error) {
+	return
+}
