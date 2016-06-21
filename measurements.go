414c73f6800b29877e3108252c70ab1521cd0151
Ollivier Robert <roberto@keltia.net>
2016-06-21 21:35:39 +0200

    Add support for info/list for measurements like probes.

--- measurements.go
+++ measurements.go
@@ -2,6 +2,9 @@ package atlas
 
 import (
 	"errors"
+	"github.com/bndr/gopencils"
+	"log"
+	"fmt"
 )
 
 var (
@@ -15,7 +18,10 @@ var (
 	}
 )
 
+// ErrInvalidMeasurementType is a new error
 var ErrInvalidMeasurementType = errors.New("invalid measurement type")
+
+// -- private
 
 // checkType verify that the type is valid
 func checkType(d Definition) (valid bool) {
@@ -35,6 +41,71 @@ func checkTypeAs(d Definition, t string) (valid bool) {
 	if checkType(d) && d.Type != t {
 		valid = false
 	}
+	return
+}
+
+// measurementList is our main answer
+type measurementList struct {
+	Count    int
+	Next     string
+	Previous string
+	Results  []Measurement
+}
+
+// fetch the given resource
+func fetchOneMeasurementPage(api *gopencils.Resource, opts map[string]string) (raw *measurementList, err error) {
+	r, err := api.Res("measurements", &raw).Get(opts)
+	if err != nil {
+		log.Printf("err: %v", err)
+		err = fmt.Errorf("%v - r:%v\n", err, r)
+	}
+	//log.Printf(">> rawlist=%+v r=%+v Next=|%s|", rawlist, r, rawlist.Next)
+	return
+}
+
+// -- public
+
+// GetMeasurement gets info for a single one
+func GetMeasurement(id int) (m *Measurement, err error) {
+	auth := WantAuth()
+	api := gopencils.Api(apiEndpoint, auth)
+	r, err := api.Res("measurements").Id(id, &m).Get()
+	if err != nil {
+		err = fmt.Errorf("%v - r:%#v\n", err, r.Raw)
+		return
+	}
+	return
+}
+
+// GetMeasurements gets info for a set
+func GetMeasurements(opts map[string]string) (m []Measurement, err error) {
+	auth := WantAuth()
+	api := gopencils.Api(apiEndpoint, auth)
+
+	rawlist, err := fetchOneMeasurementPage(api, opts)
+
+	// Empty answer
+	if rawlist.Count == 0 {
+		return nil, fmt.Errorf("empty measurement list")
+	}
+
+	var res []Measurement
+
+	res = append(res, rawlist.Results...)
+	if rawlist.Next != "" {
+		// We have pagination
+		for pn := getPageNum(rawlist.Next); rawlist.Next != ""; pn = getPageNum(rawlist.Next) {
+			opts["page"] = pn
+
+			rawlist, err = fetchOneMeasurementPage(api, opts)
+			if err != nil {
+				return
+			}
+
+			res = append(res, rawlist.Results...)
+		}
+	}
+	m = res
 	return
 }
 
@@ -85,10 +156,12 @@ func Traceroute(d Definition) (m *Measurement, err error) {
 
 // Measurement-related methods
 
-func (m *Measurement) Start() (err error) {
+// Start is for starting a given measurement
+func (m *Measurement) Start(id int) (err error) {
 	return nil
 }
 
-func (m *Measurement) Stop() (err error) {
+// Stop is for stopping a given measurement
+func (m *Measurement) Stop(id int) (err error) {
 	return nil
 }
