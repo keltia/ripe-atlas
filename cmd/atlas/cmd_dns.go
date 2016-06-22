ce51a653defb6967e1d36b41d75aa7866b80f9fc
Ollivier Robert <roberto@keltia.net>
2016-06-22 20:51:31 +0200

    Fill in struct to generate "cli" help.

--- cmd/atlas/cmd_dns.go
+++ cmd/atlas/cmd_dns.go
@@ -1,2 +1,30 @@
 package main
 
+import "github.com/codegangsta/cli"
+
+// init injects our "dns" related commands/options.
+func init() {
+	// Fill-in the various commands
+	cliCommands = append(cliCommands, cli.Command{
+		Name:        "dns",
+		Usage:       "send dns queries",
+		Description: "send DNS queries to an host/IP",
+		Flags: []cli.Flag{
+			cli.BoolFlag{
+				Name:        "ipv6",
+				Usage:       "displays only IPv6",
+				Destination: &fWant6,
+			},
+			cli.BoolFlag{
+				Name:        "ipv4",
+				Usage:       "displays only IPv4",
+				Destination: &fWant4,
+			},
+		},
+		Action: cmdDNS,
+	})
+}
+
+func cmdDNS(c *cli.Context) error {
+	return nil
+}
--- cmd/atlas/cmd_traceroute.go
+++ cmd/atlas/cmd_traceroute.go
@@ -1,3 +1,32 @@
 package main
 
+import "github.com/codegangsta/cli"
+
+// init injects our "traceroute" related commands/options.
+func init() {
+	// Fill-in the various commands
+	cliCommands = append(cliCommands, cli.Command{
+		Name:        "traceroute",
+		Aliases:     []string{"trace"},
+		Usage:       "traceroute to given host/IP",
+		Description: "Send Traceroute queries to an host/IP",
+		Flags: []cli.Flag{
+			cli.BoolFlag{
+				Name:        "ipv6",
+				Usage:       "displays only IPv6",
+				Destination: &fWant6,
+			},
+			cli.BoolFlag{
+				Name:        "ipv4",
+				Usage:       "displays only IPv4",
+				Destination: &fWant4,
+			},
+		},
+		Action: cmdTraceroute,
+	})
+}
+
+func cmdTraceroute(c *cli.Context) error {
+	return nil
+}
 
