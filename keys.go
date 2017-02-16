54779ee612920ab297435a39c68c89d781e58f48
Ollivier Robert <roberto@keltia.net>
2017-02-16 17:41:23 +0100

    /keys returns a list, not just an array of keys.

--- keys.go
+++ keys.go
@@ -4,79 +4,86 @@
 
 package atlas
 
+import ()
 import (
+	"encoding/json"
+	"fmt"
+	"github.com/sendgrid/rest"
 )
-import (
-    "encoding/json"
-    "fmt"
-    "github.com/sendgrid/rest"
-)
+
+type keyList struct {
+	Count    int
+	Next     string
+	Previous string
+	Results  []Key
+}
 
 // GetKey returns a given API key
 func GetKey(uuid string) (k Key, err error) {
-    keyEP := apiEndpoint + "/keys"
+	keyEP := apiEndpoint + "/keys"
 
-    key, ok := HasAPIKey()
+	key, ok := HasAPIKey()
 
-    // Add at least one option, the APIkey if present
-    hdrs := make(map[string]string)
-    opts := make(map[string]string)
+	// Add at least one option, the APIkey if present
+	hdrs := make(map[string]string)
+	opts := make(map[string]string)
 
-    if ok {
-        opts["key"] = key
-    }
+	if ok {
+		opts["key"] = key
+	}
 
-    req := rest.Request{
-        BaseURL:     keyEP + fmt.Sprintf("/%d", uuid),
-        Method:      rest.Get,
-        Headers:     hdrs,
-        QueryParams: opts,
-    }
+	req := rest.Request{
+		BaseURL:     keyEP + fmt.Sprintf("/%d", uuid),
+		Method:      rest.Get,
+		Headers:     hdrs,
+		QueryParams: opts,
+	}
 
-    //log.Printf("req: %#v", req)
-    r, err := rest.API(req)
-    if err != nil {
-        err = fmt.Errorf("err: %v - r:%v\n", err, r)
-        return
-    }
+	//log.Printf("req: %#v", req)
+	r, err := rest.API(req)
+	if err != nil {
+		err = fmt.Errorf("err: %v - r:%v\n", err, r)
+		return
+	}
 
-    k = Key{}
-    err = json.Unmarshal([]byte(r.Body), &k)
-    //log.Printf("json: %#v\n", p)
-    return
+	k = Key{}
+	err = json.Unmarshal([]byte(r.Body), &k)
+	//log.Printf("json: %#v\n", p)
+	return
 }
 
 // GetKeys returns all your API keys
 func GetKeys() (kl []Key, err error) {
-    keyEP := apiEndpoint + "/keys"
+	keyEP := apiEndpoint + "/keys"
 
-    key, ok := HasAPIKey()
+	key, ok := HasAPIKey()
 
-    // Add at least one option, the APIkey if present
-    hdrs := make(map[string]string)
-    opts := make(map[string]string)
+	// Add at least one option, the APIkey if present
+	hdrs := make(map[string]string)
+	opts := make(map[string]string)
 
-    if ok {
-        opts["key"] = key
-    }
+	if ok {
+		opts["key"] = key
+	}
 
-    req := rest.Request{
-        BaseURL:     keyEP,
-        Method:      rest.Get,
-        Headers:     hdrs,
-        QueryParams: opts,
-    }
+	req := rest.Request{
+		BaseURL:     keyEP,
+		Method:      rest.Get,
+		Headers:     hdrs,
+		QueryParams: opts,
+	}
 
-    //log.Printf("req: %#v", req)
-    r, err := rest.API(req)
-    if err != nil {
-        err = fmt.Errorf("err: %v - r:%v\n", err, r)
-        return
-    }
+	//log.Printf("req: %#v", req)
+	r, err := rest.API(req)
+	if err != nil {
+		err = fmt.Errorf("err: %v - r:%v\n", err, r)
+		return
+	}
 
-    kl = []Key{}
-    fmt.Printf("r: %#v", r.Body)
-    err = json.Unmarshal([]byte(r.Body), &kl)
-    //log.Printf("json: %#v\n", p)
-    return
+	list := &keyList{}
+	fmt.Printf("r: %#v", r.Body)
+	err = json.Unmarshal([]byte(r.Body), list)
+	kl = list.Results
+	//log.Printf("json: %#v\n", p)
+	return
 }
