e7ec778dfe06e4c32235d1ae35c1b50722ca6321
Ollivier Robert <roberto@keltia.net>
2017-02-20 11:34:38 +0100

    Try a more generic approach with createMeasurement().

--- measurement_subr.go
+++ measurement_subr.go
@@ -12,6 +12,57 @@ type MeasurementResp struct {
 	Measurements []int
 }
 
+// createMeasurement creates a measurement for all types
+func createMeasurement(t string, d MeasurementRequest) (m *MeasurementResp, err error) {
+    // Check that all Definition.Type are the same and compliant
+    if !checkAllTypesAs(d.Definitions, t) {
+        err = ErrInvalidMeasurementType
+        return
+    }
+
+    endPoint := apiEndpoint + fmt.Sprintf("/measurements/%s/", t)
+
+    // Add at least one option, the APIkey if present
+    hdrs := make(map[string]string)
+    opts := make(map[string]string)
+
+    key, ok := HasAPIKey()
+    if ok {
+        opts["key"] = key
+    } else {
+        err = ErrInvalidAPIKey
+        return
+    }
+
+    body, err := json.Marshal(d)
+    if err != nil {
+        return
+    }
+
+    req := rest.Request{
+        BaseURL:     endPoint,
+        Method:      rest.Post,
+        Headers:     hdrs,
+        QueryParams: opts,
+        Body:        body,
+    }
+
+    log.Printf("body: %s", body)
+    resp, err := rest.API(req)
+
+    m = &MeasurementResp{}
+    err = json.Unmarshal([]byte(resp.Body), m)
+    //r, err := api.Res(base, &resp).Post(d)
+    fmt.Printf("m: %v\nresp: %#v\nd: %v\n", m, string(resp.Body), d)
+    if err != nil {
+        err = fmt.Errorf("err: %v - m:%v\n", err, m)
+        return
+    }
+
+    return
+}
+
+
 // DNS creates a measurement
 func DNS(d MeasurementRequest) (m *MeasurementResp, err error) {
 	// Check that all Definition.Type are the same and compliant
