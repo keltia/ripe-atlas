23730a8d33c054a4c2e1fc9e4a6c2ba97827a601
Ollivier Robert <roberto@keltia.net>
2017-02-20 18:14:48 +0100

    Apply prepareRequest() there too, like in measurements.go

--- probes.go
+++ probes.go
@@ -12,24 +12,8 @@ import (
 
 // GetProbe returns data for a single probe
 func GetProbe(id int) (p *Probe, err error) {
-	probeEP := apiEndpoint + "/probes"
-
-	key, ok := HasAPIKey()
-
-	// Add at least one option, the APIkey if present
-	hdrs := make(map[string]string)
-	opts := make(map[string]string)
-
-	if ok {
-		opts["key"] = key
-	}
-
-	req := rest.Request{
-		BaseURL:     probeEP + fmt.Sprintf("/%d", id),
-		Method:      rest.Get,
-		Headers:     hdrs,
-		QueryParams: opts,
-	}
+    req := prepareRequest(fmt.Sprintf("probes/%d", id))
+    req.Method = rest.Get
 
 	//log.Printf("req: %#v", req)
 	r, err := rest.API(req)
@@ -53,40 +37,33 @@ type probeList struct {
 }
 
 // fetch the given resource
-func fetchOneProbePage(probeEP string, opts map[string]string) (raw *probeList, err error) {
-	hdrs := make(map[string]string)
-	req := rest.Request{
-		BaseURL:     probeEP,
-		Method:      rest.Get,
-		Headers:     hdrs,
-		QueryParams: opts,
-	}
+func fetchOneProbePage(opts map[string]string) (raw *probeList, err error) {
 
-	r, err := rest.API(req)
-	if err != nil {
-		err = fmt.Errorf("err: %v - r:%v\n", err, r)
-		return
-	}
+    req := prepareRequest("probes")
+    req.Method = rest.Get
 
-	raw = &probeList{}
-	err = json.Unmarshal([]byte(r.Body), raw)
-	//log.Printf(">> rawlist=%+v r=%+v Next=|%s|", rawlist, r, rawlist.Next)
-	return
+    // Do not forget to copy our options
+    for qp, val := range opts {
+        req.QueryParams[qp] = val
+    }
+
+    r, err := rest.API(req)
+    if err != nil {
+        err = fmt.Errorf("err: %v - r:%v\n", err, r)
+        return
+    }
+
+    raw = &probeList{}
+    err = json.Unmarshal([]byte(r.Body), raw)
+    //log.Printf("Count=%d raw=%v", raw.Count, r)
+    //log.Printf(">> rawlist=%+v r=%+v Next=|%s|", rawlist, r, rawlist.Next)
+    return
 }
 
 // GetProbes returns data for a collection of probes
 func GetProbes(opts map[string]string) (p []Probe, err error) {
-	probeEP := apiEndpoint + "/probes"
-
-	key, ok := HasAPIKey()
-
-	// Add APIKey if set
-	if ok {
-		opts["key"] = key
-	}
-
 	// First call
-	rawlist, err := fetchOneProbePage(probeEP, opts)
+	rawlist, err := fetchOneProbePage(opts)
 
 	// Empty answer
 	if rawlist.Count == 0 {
@@ -101,7 +78,7 @@ func GetProbes(opts map[string]string) (p []Probe, err error) {
 		for pn := getPageNum(rawlist.Next); rawlist.Next != ""; pn = getPageNum(rawlist.Next) {
 			opts["page"] = pn
 
-			rawlist, err = fetchOneProbePage(probeEP, opts)
+			rawlist, err = fetchOneProbePage(opts)
 			if err != nil {
 				return
 			}
