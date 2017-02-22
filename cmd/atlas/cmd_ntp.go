04c67abf51749e526905f801507ef0d3060666ed
Ollivier Robert <roberto@keltia.net>
2017-02-22 22:43:00 +0100

    Implement NTP measurement.

--- cmd/atlas/cmd_ntp.go
+++ cmd/atlas/cmd_ntp.go
@@ -1,6 +1,12 @@
 package main
 
-import "github.com/urfave/cli"
+import (
+	"fmt"
+	"github.com/keltia/ripe-atlas"
+	"github.com/urfave/cli"
+	"log"
+	"os"
+)
 
 // init injects our "ntp" related commands/options.
 func init() {
@@ -26,5 +32,64 @@ func init() {
 }
 
 func cmdNTP(c *cli.Context) error {
+	// By default we want both
+	if !fWant4 && !fWant6 {
+		fWant6, fWant4 = true, true
+	}
+
+	args := c.Args()
+	if len(args) == 0 {
+		log.Fatal("Error: you must specify a hostname/site!")
+	}
+
+	target := args[0]
+	var defs []atlas.Definition
+
+	if fWant4 {
+		def := atlas.Definition{
+			AF:          4,
+			Description: fmt.Sprintf("NTP v4 - %s", target),
+			Type:        "ntp",
+			Target:      target,
+		}
+		defs = append(defs, def)
+	}
+
+	if fWant6 {
+		def := atlas.Definition{
+			AF:          6,
+			Description: fmt.Sprintf("NTP v6 - %s", target),
+			Type:        "ntp",
+			Target:      target,
+		}
+		defs = append(defs, def)
+	}
+
+	req := atlas.MeasurementRequest{
+		Definitions: defs,
+		IsOneoff:    true,
+	}
+
+	// Default set of probes
+	probes := atlas.ProbeSet{
+		{
+			Requested: mycnf.PoolSize,
+			Type:      "area",
+			Value:     "WW",
+			Tags:      nil,
+		},
+	}
+
+	req.Probes = probes
+	log.Printf("req=%#v", req)
+	//str := res.Result.Display()
+
+	tls, err := atlas.NTP(req)
+	if err != nil {
+		fmt.Printf("err: %v", err)
+		os.Exit(1)
+	}
+	fmt.Printf("NTP: %#v", tls)
+
 	return nil
 }
