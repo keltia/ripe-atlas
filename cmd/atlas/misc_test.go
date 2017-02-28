328d8984c498d08a87dd6334e2aaf534e9ed18f6
Ollivier Robert <roberto@keltia.net>
2017-02-28 16:07:48 +0100

    Move utility functions into their own file, add tests.

---
+++ cmd/atlas/misc_test.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+    "testing"
+)
+
+func TestValidateFormat(t *testing.T) {
+    err := validateFormat("foo")
+    if err == true {
+        t.Error("Error: should be false", )
+    }
+
+    err = validateFormat("xml")
+    if err == false {
+        t.Error("Error: should be true", )
+    }
+}
+
+func TestAnalyzeTarget(t *testing.T) {
+    proto, site, path, port := analyzeTarget("https://atlas.ripe.net/v2/api/")
+    if proto != "https" {
+        t.Errorf("Error: proto should be https")
+    }
+    if site != "atlas.ripe.net" {
+        t.Errorf("Error: site=%s should be atlas.ripe.net", site)
+    }
+    if port != 443 {
+        t.Errorf("Error: port=%d should be 443", port)
+    }
+    if path != "/v2/api/" {
+        t.Errorf("Error: path=%s should be /v2/api/", path)
+    }
+
+
+    proto, site, path, port = analyzeTarget("http://b2b.cfmu:16443/api/")
+    if proto != "http" {
+        t.Errorf("Error: proto should be http")
+    }
+    if site != "b2b.cfmu" {
+        t.Errorf("Error: site=%s should be b2b.cfmu", site)
+    }
+    if port != 16443 {
+        t.Errorf("Error: port=%d should be 16443", port)
+    }
+    if path != "/api/" {
+        t.Errorf("Error: path=%s should be /api/", path)
+    }
+}
