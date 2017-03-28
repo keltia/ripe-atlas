385dc064bcf0ed08ad86cc88a20f771a822faf89
Ollivier Robert <roberto@keltia.net>
2017-03-28 14:37:44 +0200

    More tests, coverage is now 100%

--- config_test.go
+++ config_test.go
@@ -27,6 +27,14 @@ func TestCheckName(t *testing.T) {
 	res = checkName(file)
 	assert.EqualValues(t, "", res, "should be equal")
 
+	file = "/toto.toml"
+	res = checkName(file)
+	assert.EqualValues(t, file, res, "should be equal")
+
+	file = "toto.yaml"
+	res = checkName(file)
+	assert.EqualValues(t, "/home/foo/.toto.yaml/config.toml", res, "should be equal")
+
 	// Check plain file
 	file = "foo.toml"
 	res = checkName(file)
@@ -37,6 +45,19 @@ func TestLoadConfig(t *testing.T) {
 	file := "newconfig.toml"
 	conf, err := LoadConfig(file)
 	assert.NoError(t, err, "no file is no error")
+
+	file = "/config.yaml"
+	conf, err = LoadConfig(file)
+	assert.Error(t, err, "error")
+	assert.EqualValues(t, &Config{}, conf, "empty")
+
+	file = "perms.toml"
+	_, err = LoadConfig(file)
+	assert.Error(t, err, "no error")
+
+	file = "invalid.toml"
+	_, err = LoadConfig(file)
+	assert.Error(t, err, "no error")
 
 	file = "config.toml"
 	conf, err = LoadConfig(file)
