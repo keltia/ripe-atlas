b6a150db0c9f3e956c01a3c5f29178ec182b845d
Ollivier Robert <roberto@keltia.net>
2017-11-08 21:35:25 +0100

    Correctly interpret the parameter.  Add build flags.

--- cmd/atlas/config_unix.go
+++ cmd/atlas/config_unix.go
@@ -3,12 +3,14 @@
 // This file implements the configuration part for when you need the API
 // key to modify things in the Atlas configuration and manage measurements.
 
+// +build unix !windows
+
 package main
 
 import (
+	"os"
 	"path/filepath"
 	"strings"
-	"os"
 )
 
 var (
@@ -21,25 +23,25 @@ var (
 
 // Check the parameter for either tag or filename
 func checkName(file string) (str string) {
-	// Full path, MUST have .toml
-	if bfile := []byte(file); bfile[0] == '/' {
-		if !strings.HasSuffix(file, ".toml") {
-			str = ""
-		} else {
-			str = file
-		}
-		return
-	}
-
-	// If ending with .toml, take it literally
-	if strings.HasSuffix(file, ".toml") {
-		str = file
-		return
-	}
-
-	// Check for tag
-	if !strings.HasSuffix(file, ".toml") {
+	// Use default location
+	if file == "" {
 		str = filepath.Join(basedir, file, "config.toml")
+	} else {
+		// Full path, MUST have .toml
+		if bfile := []byte(file); bfile[0] == '/' {
+			if !strings.HasSuffix(file, ".toml") {
+				str = ""
+			} else {
+				str = file
+			}
+			return
+		}
+
+		// If ending with .toml, take it literally
+		if strings.HasSuffix(file, ".toml") {
+			str = file
+			return
+		}
 	}
 	return
 }
