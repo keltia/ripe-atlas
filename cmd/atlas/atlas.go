09442bb78c73b88799e449ff684578429acca610
Ollivier Robert <roberto@keltia.net>
2017-11-08 22:48:49 +0100

    New API w/ atlas.NewClient(). Added fDebug flag. Added support for proxy authentication. Move flags display to finalcheck().

--- cmd/atlas/atlas.go
+++ cmd/atlas/atlas.go
@@ -18,7 +18,7 @@ var (
 	fWant6 bool
 
 	// True by default
-	fWantMine bool
+	fWantMine = true
 
 	fAllProbes       bool
 	fAllMeasurements bool
@@ -39,6 +39,7 @@ var (
 	fBitCD         bool
 	fDisableDNSSEC bool
 
+	fDebug      bool
 	fVerbose    bool
 	fWantAnchor bool
 
@@ -48,11 +49,13 @@ var (
 	mycnf *Config
 
 	cliCommands []cli.Command
+
+	client *atlas.Client
 )
 
 const (
 	atlasVersion = "0.11"
-	MyName = "ripe-atlas"
+	MyName       = "ripe-atlas"
 
 	// WantBoth is the way to ask for both IPv4 & IPv6.
 	WantBoth = "64"
@@ -64,6 +67,7 @@ const (
 )
 
 // -4 & -6 are special, if neither is specified, then we turn both as true
+// Check a few other things while we are here
 func finalcheck(c *cli.Context) error {
 	if fWant4 {
 		mycnf.WantAF = Want4
@@ -83,6 +87,25 @@ func finalcheck(c *cli.Context) error {
 		mycnf.WantAF = WantBoth
 	}
 
+	// Logical
+	if fDebug {
+		fVerbose = true
+		log.Printf("config: %#v", mycnf)
+	}
+
+	// Various messages
+	if fVerbose {
+		if mycnf.APIKey != "" {
+			log.Printf("Found API key!")
+		} else {
+			log.Printf("No API key!")
+		}
+
+		if mycnf.DefaultProbe != 0 {
+			log.Printf("Found default probe: %d\n", mycnf.DefaultProbe)
+		}
+	}
+
 	return nil
 }
 
@@ -91,7 +114,7 @@ func main() {
 	cli.VersionFlag = cli.BoolFlag{Name: "version, V"}
 
 	cli.VersionPrinter = func(c *cli.Context) {
-		log.Printf("API wrapper: %s Atlas CLI: %s\n", c.App.Version, atlas.GetVersion())
+		log.Printf("API wrapper: %s Atlas API: %s\n", c.App.Version, atlas.GetVersion())
 	}
 
 	app := cli.NewApp()
@@ -107,6 +130,11 @@ func main() {
 			Name:        "format,f",
 			Usage:       "specify output format",
 			Destination: &fFormat,
+		},
+		cli.BoolFlag{
+			Name:        "debug,D",
+			Usage:       "debug mode",
+			Destination: &fDebug,
 		},
 		cli.BoolFlag{
 			Name:        "verbose,v",
@@ -145,23 +173,35 @@ func main() {
 
 	var err error
 
-	mycnf, err = LoadConfig("ripe-atlas")
-	if mycnf.APIKey != "" && err == nil {
-		atlas.SetAuth(mycnf.APIKey)
-		log.Printf("Found API key!")
-	} else {
-		log.Printf("No API key!")
-	}
-
-	// Check whether we have proxy authentication
-	mycnf.ProxyAuth, err = setupProxyAuth()
+	// Load main configuration
+	mycnf, err = LoadConfig("")
 	if err != nil {
-		log.Printf("invalid proxy auth credentials")
-		mycnf.ProxyAuth = ""
+		if fVerbose {
+			log.Printf("No configuration file found.")
+		}
 	}
 
-	if mycnf.DefaultProbe != 0 && err == nil {
-		log.Printf("Found default probe: %d\n", mycnf.DefaultProbe)
+	// Check whether we have proxy authentication (from a separate config file)
+	auth, err := setupProxyAuth()
+	if err != nil {
+		if fVerbose {
+			log.Printf("Invalid or no proxy auth credentials")
+		}
+	}
+
+	// Wondering whether to move to the Functional options pattern
+	// cf. https://dave.cheney.net/2016/11/13/do-not-fear-first-class-functions
+	client, err = atlas.NewClient(atlas.Config{
+		APIKey:       mycnf.APIKey,
+		DefaultProbe: mycnf.DefaultProbe,
+		PoolSize:     mycnf.PoolSize,
+		ProxyAuth:    auth,
+		Verbose:      fVerbose,
+	})
+
+	// No need to continue if this fails
+	if err != nil {
+		log.Fatalf("Error creating the Atlas client: %v", err)
 	}
 
 	sort.Sort(ByAlphabet(cliCommands))
