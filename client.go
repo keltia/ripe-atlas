b9d679e2f29809f4cedca1d16849fc166102c4db
Ollivier Robert <roberto@keltia.net>
2017-11-09 16:31:43 +0100

    Complete rewrite: setup http.Client once, save into context.

--- client.go
+++ client.go
@@ -1,7 +1,18 @@
 package atlas
 
+import (
+	"net/http"
+	"crypto/tls"
+	"log"
+	"net/url"
+	"fmt"
+	"time"
+)
+
 type context struct {
 	config   Config
+	client   *http.Client
+
 }
 
 var (
@@ -18,5 +29,62 @@ func NewClient(cfgs ...Config) (*Client, error) {
 	for _, cfg := range cfgs {
 		ctx.config = cfg
 	}
+	ctx.client = addHTTPClient(ctx)
 	return client, nil
+}
+
+func getProxy(req *http.Request) (uri *url.URL, err error) {
+	uri, err = http.ProxyFromEnvironment(req)
+	if err != nil {
+		log.Printf("no proxy in environment")
+		uri = &url.URL{}
+	} else if uri == nil {
+		log.Println("No proxy configured or url excluded")
+	}
+	return
+}
+
+func setupTransport(ctx *context) (*http.Transport, error) {
+	/*
+    Proxy code taken from https://github.com/LeoCBS/poc-proxy-https/blob/master/main.go
+    Analyse endPoint to check proxy stuff
+	*/
+	req, err := http.NewRequest("HEAD", apiEndpoint, nil)
+	if err != nil {
+		log.Printf("error: transport: %v", err)
+		return nil, err
+	}
+
+	// Get proxy URL
+	proxyURL, err := getProxy(req)
+	if err != nil {
+		if ctx.config.Verbose {
+			log.Println("no proxy defined")
+		}
+	}
+
+	myurl, err := url.Parse(apiEndpoint)
+
+	req.Header.Set("Host", myurl.Host)
+	req.Header.Add("User-Agent", fmt.Sprintf("ripe-atlas/%s", ourVersion))
+
+	if ctx.config.ProxyAuth != "" {
+		req.Header.Add("Proxy-Authorization", ctx.config.ProxyAuth)
+	}
+
+	transport := &http.Transport{
+		Proxy:              http.ProxyURL(proxyURL),
+		TLSClientConfig:    &tls.Config{InsecureSkipVerify: true},
+		ProxyConnectHeader: req.Header,
+	}
+
+	return transport, nil
+}
+
+func addHTTPClient(ctx *context) *http.Client {
+	transport, err := setupTransport(ctx)
+	if err != nil {
+		log.Fatalf("unable to create httpclient: %v", err)
+	}
+	return &http.Client{Transport: transport, Timeout: 20 * time.Second}
 }
