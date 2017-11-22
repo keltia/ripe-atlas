20cb44949ff15a40436917feb3a48d70eddbc395
Ollivier Robert <roberto@keltia.net>
2017-11-22 23:56:05 +0100

    Minimal test for mergeOptions().

---
+++ cmd/atlas/flag_test.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"testing"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestMergeOptions(t *testing.T) {
+
+	var (
+		v1, v2, v3 string
+	)
+
+	v1 = "aa"
+	v2 = "bb"
+	v3 = "cc"
+
+	foo := map[string]*string{
+		"a": &v1,
+		"b": &v2,
+	}
+	bar := map[string]string{
+		"c": v3,
+	}
+
+	res := map[string]string{
+		"a": "aa",
+		"b": "bb",
+		"c": "cc",
+	}
+	baz := mergeOptions(bar, foo)
+	assert.EqualValues(t, res, baz, "equal")
+}
