c827a61422d8bcacfd8e1d2272a33c970ae384d3
Ollivier Robert <roberto@keltia.net>
2018-09-10 22:36:27 +0200

    APIError is an error.

--- errors.go
+++ errors.go
@@ -1,6 +1,8 @@
 package atlas
 
-import "errors"
+import (
+	"github.com/pkg/errors"
+)
 
 // ErrInvalidMeasurementType is a new error
 var ErrInvalidMeasurementType = errors.New("invalid measurement type")
@@ -10,3 +12,12 @@ var ErrInvalidAPIKey = errors.New("invalid API key")
 
 // ErrAPIKeyIsMandatory is returned when a call need one
 var ErrAPIKeyIsMandatory = errors.New("API call requires an API key")
+
+// ErrInvalidAPIAnswer
+var ErrInvalidError = errors.New("Invalid request")
+
+var lastError APIError
+
+func (e APIError) Error() string {
+	return e.Err.Detail
+}
