6dbf126b8af5a8521625e1727304c0e267777c69
Ollivier Robert <roberto@keltia.net>
2018-09-18 16:31:01 +0200

    Add test for Client_FetchResult().

---
+++ results_test.go
@@ -0,0 +1,38 @@
+package atlas
+
+import (
+	"net/url"
+	"testing"
+
+	"github.com/h2non/gock"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestClient_FetchResult(t *testing.T) {
+	defer gock.Off()
+
+	myurl, _ := url.Parse("https://example.com/fetch")
+
+	gock.New("https://example.com").
+		Get("/fetch").
+		Reply(200).
+		BodyString(`{"some": "results"`)
+
+	c := Before(t)
+	c.level = 2
+	require.NotNil(t, c)
+	require.NotNil(t, c.client)
+
+	opts := make(map[string]string)
+	c.mergeGlobalOptions(opts)
+
+	gock.InterceptClient(c.client)
+	defer gock.RestoreClient(c.client)
+
+	body, err := c.FetchResult(myurl.String())
+	assert.NoError(t, err)
+	assert.NotEmpty(t, body)
+
+	assert.Equal(t, `{"some": "results"`, body)
+}
