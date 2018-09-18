62ddad6420e28eaa6e2e815acf630580ff64ced6
Ollivier Robert <roberto@keltia.net>
2018-09-18 16:34:11 +0200

    And another for error case.

--- results_test.go
+++ results_test.go
@@ -36,3 +36,30 @@ func TestClient_FetchResult(t *testing.T) {
 
 	assert.Equal(t, `{"some": "results"`, body)
 }
+
+func TestClient_FetchResult2(t *testing.T) {
+	defer gock.Off()
+
+	myurl, _ := url.Parse("https://example.com/fetch")
+
+	gock.New("https://example.com").
+		Get("/fetch").
+		MatchHeader("foo", "bar").
+		Reply(200).
+		BodyString(`{"some": "results"`)
+
+	c := Before(t)
+	c.level = 2
+	require.NotNil(t, c)
+	require.NotNil(t, c.client)
+
+	opts := make(map[string]string)
+	c.mergeGlobalOptions(opts)
+
+	gock.InterceptClient(c.client)
+	defer gock.RestoreClient(c.client)
+
+	body, err := c.FetchResult(myurl.String())
+	assert.Error(t, err)
+	assert.Empty(t, body)
+}
