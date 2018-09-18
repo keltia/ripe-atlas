a9316e7e696b15b5c080ad3bfbb7ac5a8dc8c565
Ollivier Robert <roberto@keltia.net>
2018-09-18 22:00:27 +0200

    Sprinkle some debug() around. Fix interaction with handleAPIResponse().

--- probes.go
+++ probes.go
@@ -21,23 +21,25 @@ func (c *Client) GetProbe(id int) (p *Probe, err error) {
 	c.mergeGlobalOptions(opts)
 
 	req := c.prepareRequest("GET", fmt.Sprintf("probes/%d", id), opts)
+	c.debug("req=%#v", req)
 
 	resp, err := c.call(req)
-	//log.Printf("resp: %#v - err: %#v", resp, err)
+	c.debug("resp: %#v - err: %#v", resp, err)
 	if err != nil {
-		c.verbose("API error: %v", err)
-		_, err = c.handleAPIResponse(resp)
-		if err != nil {
-			return &Probe{}, errors.Wrap(err, "GetProbe")
-		}
+		return &Probe{}, errors.Wrap(err, "GetProbe/call")
+	}
+
+	// We may have all http errors here but the request did succeed
+	c.debug("http.code=%d", resp.StatusCode)
+
+	body, err := c.handleAPIResponse(resp)
+	if err != nil {
+		return &Probe{}, errors.Wrap(err, "GetProbe")
 	}
 
 	p = &Probe{}
-	body, _ := ioutil.ReadAll(resp.Body)
-	defer resp.Body.Close()
-
 	err = json.Unmarshal(body, p)
-	//log.Printf("json: %#v\n", p)
+	c.debug("json: %#v\n", p)
 	return
 }
 
