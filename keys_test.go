0f4b1d6a1d6c8e5922d0cdbd07c7f081fa7760fb
Ollivier Robert <roberto@keltia.net>
2018-09-19 00:22:17 +0200

    Add tests for GetKet() & GetKeys().

--- keys_test.go
+++ keys_test.go
@@ -10,10 +10,67 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func TestClient_GetKeys_Badkey(t *testing.T) {
+func TestClient_GetKey_BadKey(t *testing.T) {
 	defer gock.Off()
 
-	//myurl, _ := url.Parse(apiEndpoint)
+	opts1 := map[string]string{
+		"key":  "foobar",
+		"uuid": "blah",
+	}
+
+	gock.New(apiEndpoint).
+		Get("/keys/" + opts1["uuid"]).
+		MatchParams(opts1).
+		Reply(403).
+		BodyString(`{"error":{"status":403,"code":104,"detail":"The provided API key does not exist","title":"Forbidden"}}`)
+
+	c := Before(t)
+
+	gock.InterceptClient(c.client)
+	defer gock.RestoreClient(c.client)
+
+	rp, err := c.GetKey("27768f56-bb86-11e8-b7d0-27cd18d24377")
+
+	assert.Error(t, err)
+	assert.Empty(t, rp)
+
+}
+
+func TestClient_GetKey(t *testing.T) {
+	defer gock.Off()
+
+	opts1 := map[string]string{
+		"key": "foobar",
+	}
+
+	fk, err := ioutil.ReadFile("testdata/single-key.json")
+	assert.NoError(t, err)
+
+	gock.New(apiEndpoint).
+		Get("/keys/" + opts1["uuid"]).
+		MatchParams(opts1).
+		Reply(200).
+		BodyString(string(fk))
+
+	c := Before(t)
+
+	gock.InterceptClient(c.client)
+	defer gock.RestoreClient(c.client)
+
+	var jfk Key
+
+	err = json.Unmarshal(fk, &jfk)
+	require.NoError(t, err)
+
+	rp, err := c.GetKey("27768f56-bb86-11e8-b7d0-27cd18d24377")
+
+	assert.NoError(t, err)
+	assert.NotEmpty(t, rp)
+	assert.EqualValues(t, jfk, rp)
+}
+
+func TestClient_GetKeys_Badkey(t *testing.T) {
+	defer gock.Off()
 
 	gock.New(apiEndpoint).
 		Get("/keys").
@@ -36,10 +93,10 @@ func TestClient_GetKeys_Badkey(t *testing.T) {
 func TestClient_GetKeys(t *testing.T) {
 	defer gock.Off()
 
-	ft, err := ioutil.ReadFile("testdata/keys.json")
+	ft, err := ioutil.ReadFile("testdata/keys-list.json")
 	assert.NoError(t, err)
 
-	fk, err := ioutil.ReadFile("testdata/key.json")
+	fk, err := ioutil.ReadFile("testdata/keys.json")
 	assert.NoError(t, err)
 
 	gock.New(apiEndpoint).
@@ -54,13 +111,13 @@ func TestClient_GetKeys(t *testing.T) {
 	defer gock.RestoreClient(c.client)
 
 	opts := map[string]string{}
-	rp, err := c.GetKeys(opts)
 
 	var jfk []Key
 
 	err = json.Unmarshal(fk, &jfk)
 	require.NoError(t, err)
 
+	rp, err := c.GetKeys(opts)
 	assert.NoError(t, err)
 	assert.NotEmpty(t, rp)
 	assert.EqualValues(t, jfk, rp)
