3d7454b5ca9c179c7f3d463a3366a484923b931a
Ollivier Robert <roberto@keltia.net>
2018-09-18 15:34:53 +0200

    Add debug() and level support to verbose().

--- log.go
+++ log.go
@@ -1,7 +1,14 @@
 package atlas
 
 func (c *Client) verbose(fmt string, args ...interface{}) {
-	if c.config.Verbose {
+	if c.level >= 1 {
 		c.log.Printf(fmt, args...)
+	}
+}
+
+// debug displays only if fDebug is set
+func (c *Client) debug(str string, args ...interface{}) {
+	if c.level >= 2 {
+		c.log.Printf(str, args...)
 	}
 }
