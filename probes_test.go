f6a735585613ee09122757f6f08552df5d6b7a13
Ollivier Robert <roberto@keltia.net>
2018-09-18 21:57:47 +0200

    Fix test when 403 and add a 200 test.

--- probes_test.go
+++ probes_test.go
@@ -1,32 +1,71 @@
 package atlas
 
 import (
+	"encoding/json"
+	"io/ioutil"
 	"testing"
+
+	"github.com/stretchr/testify/require"
+
+	"github.com/h2non/gock"
+	"github.com/stretchr/testify/assert"
 )
 
+func TestClient_GetProbe_Badkey(t *testing.T) {
+	defer gock.Off()
+
+	//myurl, _ := url.Parse(apiEndpoint)
+
+	gock.New(apiEndpoint).
+		Get("/probes/0").
+		MatchParam("key", "foobar").
+		Reply(403).
+		BodyString(`{"error":{"status":403,"code":104,"detail":"The provided API key does not exist","title":"Forbidden"}}`)
+
+	c := Before(t)
+	c.level = 2
+
+	gock.InterceptClient(c.client)
+	defer gock.RestoreClient(c.client)
+
+	//myerr := "status: 403 code: 104 - r:The provided API key does not exist\nerrors: []"
+
+	rp, err := c.GetProbe(0)
+
+	t.Logf("rp=%#v", rp)
+	assert.Error(t, err)
+	assert.Empty(t, rp)
+}
+
 func TestClient_GetProbe(t *testing.T) {
-	/*	defer gock.Off()
+	defer gock.Off()
 
-		//myurl, _ := url.Parse(apiEndpoint)
+	//myurl, _ := url.Parse(apiEndpoint)
 
-		gock.New(apiEndpoint).
-			Get("/probes/0").
-			MatchParam("key", "foobar").
-			Reply(403).
-			BodyString(`{"error":{"status":403,"code":104,"detail":"The provided API key does not exist","title":"Forbidden"}}`)
+	ft, err := ioutil.ReadFile("testdata/probe-0.json")
+	assert.NoError(t, err)
 
-		c := Before(t)
+	gock.New(apiEndpoint).
+		Get("/probes/0").
+		MatchParam("key", "foobar").
+		Reply(200).
+		BodyString(string(ft))
 
-		gock.InterceptClient(c.client)
-		defer gock.RestoreClient(c.client)
+	c := Before(t)
+	c.level = 2
 
-		myerr := "status: 403 code: 104 - r:The provided API key does not exist\nerrors: []"
+	gock.InterceptClient(c.client)
+	defer gock.RestoreClient(c.client)
 
-		rp, err := c.GetProbe(0)
+	rp, err := c.GetProbe(0)
 
-		t.Logf("rp=%#v", rp)
-		assert.Error(t, err)
-		assert.Nil(t, rp)
-		assert.Equal(t, myerr, err.Error())
-	*/
+	var jft Probe
+
+	err = json.Unmarshal(ft, &jft)
+	require.NoError(t, err)
+
+	t.Logf("rp=%#v", rp)
+	assert.NoError(t, err)
+	assert.NotEmpty(t, rp)
+	assert.EqualValues(t, &jft, rp)
 }
