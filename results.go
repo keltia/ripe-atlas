93ebdf0a2087ffd738b65ba167cef92f85aea609
Ollivier Robert <roberto@keltia.net>
2018-09-18 16:31:15 +0200

    Add some debug.

--- results.go
+++ results.go
@@ -2,6 +2,8 @@ package atlas
 
 import (
 	"io/ioutil"
+
+	"github.com/pkg/errors"
 )
 
 // FetchResult downloads result for a given measurement
@@ -15,16 +17,18 @@ func (c *Client) FetchResult(url string) (string, error) {
 
 	req := c.prepareRequest("FETCH", url, opts)
 
-	//log.Printf("req: %#v", req)
+	c.debug("req=%#v", req)
+	c.debug("url=%s", req.URL.String())
+
 	resp, err := c.call(req)
 	err = c.handleAPIResponse(resp)
+	c.debug("resp=%#v", resp)
 	if err != nil {
-		return "", err
+		return "", errors.Wrap(err, "fetchresult/call")
 	}
 
 	body, err := ioutil.ReadAll(resp.Body)
 	defer resp.Body.Close()
 
-	//log.Printf("json: %#v\n", m)
 	return string(body), nil
 }
