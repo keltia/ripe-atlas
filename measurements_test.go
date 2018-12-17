612b7f2d597e910a26574a1089e341472a1405fa
Ollivier Robert <roberto@keltia.net>
2018-12-17 11:43:06 +0100

    Tests for Get/DeleteMeasurement().

--- measurements_test.go
+++ measurements_test.go
@@ -1,8 +1,13 @@
 package atlas
 
 import (
-	"github.com/stretchr/testify/assert"
+	"encoding/json"
+	"fmt"
+	"net/url"
 	"testing"
+
+	"github.com/h2non/gock"
+	"github.com/stretchr/testify/assert"
 )
 
 func TestCheckType(t *testing.T) {
@@ -49,3 +54,119 @@ func TestCheckAllTypesAs(t *testing.T) {
 	assert.EqualValues(t, true, valid, "should be true")
 }
 
+func TestClient_GetMeasurement(t *testing.T) {
+
+}
+
+func TestClient_DeleteMeasurement_Nokey(t *testing.T) {
+	defer gock.Off()
+
+	pkNumber := 666
+	myurl, _ := url.Parse(apiEndpoint)
+
+	myrp := `{"error":{"status":403,"code":104,"detail":"The provided API key does not exist","title":"Forbidden"}}`
+
+	gock.New(apiEndpoint).
+		Delete(fmt.Sprintf("measurements/%d/", pkNumber)).
+		MatchHeaders(map[string]string{
+			"host":       myurl.Host,
+			"user-agent": fmt.Sprintf("ripe-atlas/%s", ourVersion),
+		}).
+		Reply(403).
+		BodyString(myrp)
+
+	c := Before(t)
+
+	gock.InterceptClient(c.client)
+	defer gock.RestoreClient(c.client)
+
+	err := c.DeleteMeasurement(pkNumber)
+	assert.Error(t, err)
+}
+
+func TestClient_DeleteMeasurement_Ok(t *testing.T) {
+	defer gock.Off()
+
+	pkNumber := 666
+	myurl, _ := url.Parse(apiEndpoint)
+
+	myrp := Measurement{ID: 666}
+	jrp, err := json.Marshal(myrp)
+
+	gock.New(apiEndpoint).
+		Delete(fmt.Sprintf("measurements/%d/", pkNumber)).
+		MatchParam("key", "foobar").
+		MatchHeaders(map[string]string{
+			"host":       myurl.Host,
+			"user-agent": fmt.Sprintf("ripe-atlas/%s", ourVersion),
+		}).
+		Reply(200).
+		BodyString(string(jrp))
+
+	c := Before(t)
+
+	gock.InterceptClient(c.client)
+	defer gock.RestoreClient(c.client)
+
+	err = c.DeleteMeasurement(pkNumber)
+	assert.NoError(t, err)
+}
+
+func TestClient_GetMeasurement_Nokey(t *testing.T) {
+	defer gock.Off()
+
+	pkNumber := 666
+	myurl, _ := url.Parse(apiEndpoint)
+
+	myrp := `{"error":{"status":403,"code":104,"detail":"The provided API key does not exist","title":"Forbidden"}}`
+
+	gock.New(apiEndpoint).
+		Get(fmt.Sprintf("measurements/%d/", pkNumber)).
+		MatchHeaders(map[string]string{
+			"host":       myurl.Host,
+			"user-agent": fmt.Sprintf("ripe-atlas/%s", ourVersion),
+		}).
+		Reply(403).
+		BodyString(myrp)
+
+	c := Before(t)
+	c.level = 2
+
+	gock.InterceptClient(c.client)
+	defer gock.RestoreClient(c.client)
+
+	rp, err := c.GetMeasurement(pkNumber)
+	assert.Error(t, err)
+	assert.Empty(t, rp)
+}
+
+func TestClient_GetMeasurement_Ok(t *testing.T) {
+	defer gock.Off()
+
+	pkNumber := 666
+	myurl, _ := url.Parse(apiEndpoint)
+
+	myrp := Measurement{ID: 666}
+	jrp, err := json.Marshal(myrp)
+
+	gock.New(apiEndpoint).
+		Get(fmt.Sprintf("measurements/%d/", pkNumber)).
+		MatchParam("key", "foobar").
+		MatchHeaders(map[string]string{
+			"host":       myurl.Host,
+			"user-agent": fmt.Sprintf("ripe-atlas/%s", ourVersion),
+		}).
+		Reply(200).
+		BodyString(string(jrp))
+
+	c := Before(t)
+	c.level = 2
+
+	gock.InterceptClient(c.client)
+	defer gock.RestoreClient(c.client)
+
+	rp, err := c.GetMeasurement(pkNumber)
+	assert.NoError(t, err)
+	assert.NotEmpty(t, rp)
+	assert.Equal(t, pkNumber, rp.ID)
+}
